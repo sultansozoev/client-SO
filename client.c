42cc89b1874a377e47ac9a43d0b5cb948d78db4d
Sultan <sozoev.sultan@gmail.com>
2024-05-24 12:43:37 +0200

    some changes

--- client.c
+++ client.c
@@ -128,10 +128,9 @@ void func(int sockfd)
 
 int main()
 {
-    int sockfd, connfd;
-    struct sockaddr_in servaddr, cli;
+    int sockfd;
+    struct sockaddr_in servaddr;
 
-    // socket create and verification
     sockfd = socket(AF_INET, SOCK_STREAM, 0);
     if (sockfd == -1) {
         printf("socket creation failed...\n");
@@ -141,12 +140,10 @@ int main()
         printf("Socket successfully created..\n");
     bzero(&servaddr, sizeof(servaddr));
 
-    // assign IP, PORT
     servaddr.sin_family = AF_INET;
     servaddr.sin_addr.s_addr = inet_addr("127.0.0.1");
     servaddr.sin_port = htons(PORT);
 
-    // connect the client socket to server socket
     if (connect(sockfd, (SA*)&servaddr, sizeof(servaddr))
         != 0) {
         printf("connection with the server failed...\n");
@@ -155,9 +152,6 @@ int main()
     else
         printf("connected to the server..\n");
 
-    // function for chat
     func(sockfd);
-
-    // close the socket
     close(sockfd);
 }
